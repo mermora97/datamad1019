6ba8bab8386fa36bf5b549bf47a0388fbd1703a0
Mercedes <mermora97@gmail.com>
2019-10-30 16:52:03 +0100

    Lab MySQL solutions

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,4 @@
+UPDATE `lab_mysql`.`SalesPersons` SET `Store` = 'Miami' WHERE (`ID` = '4');
+UPDATE `lab_mysql`.`Customers` SET `Email` = 'ppicasso@gmail.com' WHERE (`ID` = '0');
+UPDATE `lab_mysql`.`Customers` SET `Email` = 'lincoln@us.gov' WHERE (`ID` = '1');
+UPDATE `lab_mysql`.`Customers` SET `Email` = 'hello@napoleon.me' WHERE (`ID` = '2');

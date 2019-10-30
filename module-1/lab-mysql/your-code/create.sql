939065f144ee308da171146a772c3ec947c1a690
Mercedes <mermora97@gmail.com>
2019-10-30 16:36:21 +0100

    finally

--- module-1/lab-mysql/your-code/create.sql
+++ module-1/lab-mysql/your-code/create.sql
@@ -14,87 +14,87 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema lab_mysql
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE DATABASE IF NOT EXISTS `lab_mysql`;
+
 USE `lab_mysql` ;
 
 -- -----------------------------------------------------
 -- Table `lab_mysql`.`Cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Cars` (
-  `ID` INT(11) NOT NULL,
-  `VIN` VARCHAR(45) NOT NULL,
-  `Manufacturer` VARCHAR(20) NOT NULL,
-  `Model` VARCHAR(45) NOT NULL,
-  `Year` YEAR(4) NOT NULL,
-  `Color` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`ID`))
+CREATE TABLE IF NOT EXISTS lab_mysql.Cars (
+  ID INT NOT NULL,
+  VIN VARCHAR(45) NOT NULL,
+  Manufacturer VARCHAR(20) NOT NULL,
+  Model VARCHAR(45) NOT NULL,
+  Year YEAR(4) NOT NULL,
+  Color VARCHAR(20) NOT NULL,
+  PRIMARY KEY (ID))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`Customers`
+-- Table lab_mysql.Customers
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Customers` (
-  `ID` INT NOT NULL,
-  `Customer id` INT(11) NOT NULL,
-  `Name` VARCHAR(30) NOT NULL,
-  `Phone` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NULL DEFAULT NULL,
-  `Address` VARCHAR(45) NOT NULL,
-  `City` VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS lab_mysql.Customers (
+  ID INT NOT NULL,
+  `Customer id` INT NOT NULL,
+  Name VARCHAR(30) NOT NULL,
+  Phone VARCHAR(45) NOT NULL,
+  Email VARCHAR(45) NULL DEFAULT NULL,
+  Address VARCHAR(45) NOT NULL,
+  City VARCHAR(20) NOT NULL,
   `State/Province` VARCHAR(20) NOT NULL,
-  `Country` VARCHAR(20) NOT NULL,
-  `Postal` INT NOT NULL,
-  PRIMARY KEY (`ID`))
+  Country VARCHAR(20) NOT NULL,
+  Postal INT NOT NULL,
+  PRIMARY KEY (ID))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`SalesPersons`
+-- Table lab_mysql.SalesPersons
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`SalesPersons` (
-  `ID` INT NOT NULL,
-  `Staff ID` INT(11) NOT NULL,
-  `Name` VARCHAR(30) NOT NULL,
-  `Store` VARCHAR(30) NOT NULL,
-  `SalesPersons_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`))
+CREATE TABLE IF NOT EXISTS lab_mysql.SalesPersons (
+  ID INT NOT NULL,
+  `Staff ID` INT NOT NULL,
+  Name VARCHAR(30) NOT NULL,
+  Store VARCHAR(30) NOT NULL,
+  PRIMARY KEY (ID))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`Invoices`
+-- Table lab_mysql.Invoices
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Invoices` (
-  `ID` INT(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS lab_mysql.Invoices (
+  ID INT NOT NULL,
   `Invoice Number` VARCHAR(30) NOT NULL,
-  `Date` DATETIME NOT NULL,
-  `Customer` INT(11) NOT NULL,
-  `Sales Person` INT(11) NOT NULL,
-  `Car` INT NOT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `Customer_idx` (`Customer` ASC) VISIBLE,
+  Date DATE NOT NULL,
+  Car INT NOT NULL,
+  Customer INT NOT NULL,
+  `Sales Person` INT NOT NULL,
+  PRIMARY KEY (ID),
+  INDEX Customer_idx (Customer ASC) VISIBLE,
   INDEX `Sales Person_idx` (`Sales Person` ASC) VISIBLE,
-  INDEX `Car_idx` (`Car` ASC) VISIBLE,
-  CONSTRAINT `Customer`
-    FOREIGN KEY (`Customer`)
-    REFERENCES `lab_mysql`.`Customers` (`ID`)
+  INDEX Car_idx (Car ASC) VISIBLE,
+  CONSTRAINT Customer
+    FOREIGN KEY (Customer)
+    REFERENCES lab_mysql.Customers (ID)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `Sales Person`
     FOREIGN KEY (`Sales Person`)
-    REFERENCES `lab_mysql`.`SalesPersons` (`ID`)
+    REFERENCES lab_mysql.SalesPersons (ID)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
-  CONSTRAINT `Car`
-    FOREIGN KEY (`Car`)
-    REFERENCES `lab_mysql`.`Cars` (`ID`)
+  CONSTRAINT Car
+    FOREIGN KEY (Car)
+    REFERENCES lab_mysql.Cars (ID)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
